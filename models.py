4f38208691c228e66bcf4c353a9b47f998d02f73
Ryan McKenna <rmckenna21@gmail.com>
2020-03-28 13:38:35 -0400

    simple model

--- models.py
+++ models.py
@@ -11,34 +11,6 @@ def train(data, features):
     model.fit(X, y)
     return model
 
-def generate_heatmap(features):
-    """
-    Generate HeatMap for given features
-    :param features: a dictionary of features. e.g., to get heatmap for Mike Trout
-        features = { 'batter' : 545361 }
-
-    :return: the heatmap json object
-    """
-    model = pickle.load(open('lgbm.pkl','rb'))
-    x = pd.DataFrame(np.linspace(-2, 2), columns=['plate_x'])
-    y = pd.DataFrame(np.linspace(0, 5), columns=['plate_z'])
-    x['key'] = 0
-    y['key'] = 0
-    df = pd.merge(x, y, on='key').drop(columns='key')
-    for col in features:
-        df[col] = features[col]
-        if type(features[col]) in [str, int, bool]:
-            df[col] = df[col].astype('category')
-
-    p = model.predict_proba(df)[:,1]
-
-    json = {}
-    json['zone'] = [-0.75, 1.5, 0.75, 3.5]
-    json['x'] = list(df.plate_x)
-    json['y'] = list(df.plate_z)
-    json['heat'] = list(p)
-    return json
-    
 if __name__ == '__main__':
     data = pickle.load(open('data.pkl','rb'))
     data = data[data.events.notnull()]
--- server/app.py
+++ server/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import pickle
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -9,6 +12,34 @@ def index():
 @app.route('/heatmap', methods=["POST"])
 def heatmap():
     # Ryan, this is where you would plug in your function to generate the heatmap
-    with open('../trout.json', 'r') as file:
-        heatmap = file.read()
-        return heatmap
+    features = { 'batter' : 545361 }
+    return generate_heatmap(features)
+
+def generate_heatmap(features):
+    """
+    Generate HeatMap for given features
+    :param features: a dictionary of features. e.g., to get heatmap for Mike Trout
+        features = { 'batter' : 545361 }
+
+    :return: the heatmap json object
+    """
+    model = pickle.load(open('../lgbm.pkl','rb'))
+    x = pd.DataFrame(np.linspace(-2, 2), columns=['plate_x'])
+    y = pd.DataFrame(np.linspace(0, 5), columns=['plate_z'])
+    x['key'] = 0
+    y['key'] = 0
+    df = pd.merge(x, y, on='key').drop(columns='key')
+    for col in features:
+        df[col] = features[col]
+        if type(features[col]) in [str, int, bool]:
+            df[col] = df[col].astype('category')
+
+    p = model.predict_proba(df)[:,1]
+
+    json = {}
+    json['zone'] = [-0.75, 1.5, 0.75, 3.5]
+    json['x'] = list(df.plate_x)
+    json['y'] = list(df.plate_z)
+    json['heat'] = list(p)
+    return json
+
